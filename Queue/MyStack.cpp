b8371d4e74d6a0e5a78c46582aca0b9b82823a05
lierfengmei <lifengmei89@sina.com>
2017-03-09 17:28:26 +0800

    work off

---
+++ Queue/MyStack.cpp
@@ -0,0 +1,13 @@
+
+template<class T> class CStack
+{
+  public:
+    CStack();
+    ~CStack();
+   
+
+  private:
+    queue<T> queue1;
+    queue<T> queue2; 
+
+};
---
+++ Queue/README.md
@@ -0,0 +1,8 @@
+###1. 用两个栈实现一个队列Queue
+#### Queue.cpp
+
+
+###2. 用两个队列实现一个栈MyStack
+#### MyStack.cpp
+
+
